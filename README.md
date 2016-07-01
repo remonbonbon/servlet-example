a2c6a2f23c5e4d258e33554ff8396f19b3dbb87d
remonbonbon <remonbonbon@users.noreply.github.com>
2016-07-02 00:21:33 +0900

    Wrote how-to

---
+++ README.md
@@ -0,0 +1,7 @@
+# servlet-example
+Simple servlet example
+
+1. Download Oracle JDK 8 and extract to `../jdk1.8`
+2. Download Apache Tomcat 9 and extract to `../apache-tomcat-9`
+3. Execute `run.sh`
+4. Open `http://<your host>:8080/servlet-example`
--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 PWD=`pwd`
+PRGNAME=`basename $PWD`
 
 # Setup environment
-export JAVA_HOME=$PWD/../jdk1.8.0_92
-export CATALINA_HOME=$PWD/../apache-tomcat-9.0.0.M8
+export JAVA_HOME=$PWD/../jdk1.8
+export CATALINA_HOME=$PWD/../apache-tomcat-9
 
 # Compile
 export CLASSPATH=$CATALINA_HOME/lib/servlet-api.jar
@@ -14,7 +15,7 @@ $JAVA_HOME/bin/javac \
   || { echo "Compile failed"; exit 1; }
 
 # Deploy
-ln -sf $PWD/WebContent/ $CATALINA_HOME/webapps/servlet-example
+ln -sf $PWD/WebContent/ $CATALINA_HOME/webapps/$PRGNAME
 
 # Run on background
 #$CATALINA_HOME/bin/shutdown.sh
